d94f0af8101907308e1155f423f88967db32eda0
Vic <vic@Vics-MacBook-Pro.local>
2026-07-07 17:10:33 +0300

    removing redundant configs

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import path from 'node:path';
-import fs from 'node:fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle('Cats API Example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
-    .build();
-
-  // Create document object
-  const document = SwaggerModule.createDocument(app, config);
-
-  const outputPath = path.resolve(process.cwd(), 'openapi.json');
-  fs.writeFileSync(outputPath, JSON.stringify(document, null, 2), {
-    encoding: 'utf8',
-  });
-
-  // Setup the Swagger UI route
-  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
